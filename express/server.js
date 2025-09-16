2f56ab2b1611ad26273daa6c75ad91c5bcaff4ac
STRO09 <sagarjanjoted123@gmail.com>
2025-09-16 09:45:55 +0530

    express api with fetch api

--- express/server.js
+++ express/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 
 let users = [
   { id: 1, name: "Sagar", salary: 50000 },
